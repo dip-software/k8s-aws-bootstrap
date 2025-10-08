c135ca34d3abd9d917b569361f4763073e2ac60d
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2025-10-08 10:53:37 +0200

    Add renovate
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

---
+++ .github/renovate-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  username: "renovate[bot]",
+  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+  onboarding: false,
+  platform: "github",
+  forkProcessing: "disabled",
+  dryRun: null,
+  repositories: ["dip-software/k8s-aws-bootstrap"],
+  enabledManagers: ["custom.regex"],
+  customManagers: [
+    {
+      customType: "regex",
+      matchStringsStrategy: "any",
+      managerFilePatterns: [
+        "/.github/workflows/.*.(yaml|yml)/",
+        "/.github/actions/.*.(yaml|yml)/"
+      ],
+      matchStrings: [
+        '# renovate:\\s+?datasource=(?<datasource>\\S+?)\\s+?depName=(?<depName>\\S+?)\\s+?(default|(?i:.*version))\\s?(:|=|:=|\\?=)\\s?"?(?<currentValue>\\S+?)"\\s',
+      ],
+    },
+  ],
+  packageRules: [
+    {
+      matchDatasources: ["github-releases"],
+    },
+  ],
+};
